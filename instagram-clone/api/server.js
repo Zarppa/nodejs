9f69032420e48eaed0cf8210c28445632e06928f
Guilherme Palma <gomes.lmc@gmail.com>
2018-02-02 10:45:53 -0200

    Instagram Clone - Metodos GET e PUT MongoDB API

--- instagram-clone/api/server.js
+++ instagram-clone/api/server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
-	mongodb = require('mongodb');
+	mongodb = require('mongodb'),
+	objectId = require('mongodb').ObjectId;
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.get('', function(req, res){
 	res.send({msg:'Olá'});
 });
 
+//POST
 app.post('/api', function(req, res){
 
 	var dados = req.body;
@@ -42,6 +44,61 @@ app.post('/api', function(req, res){
 			});
 		});
 });
+//GET ALL
+app.get('/api', function(req, res){
+	
+	db.open( function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.find().toArray(function(err, results){
+				if(err){
+					res.json(err);
+				}else {
+					res.json(results);
+				}
+				mongoclient.close();
+				});
+			});
+		});
+	});
+//GET by ID
+app.get('/api/:id', function(req, res){
+	
+	db.open( function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.find(objectId(req.params.id)).toArray(function(err, results){
+				if(err){
+					res.json(err);
+				}else {
+					res.json(results);
+				}
+				mongoclient.close();
+				});
+			});
+		});
+	});
+
+//PUT by ID
+app.put('/api/:id', function(req, res){
+	
+	db.open( function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.update(
+				{ _id : objectId(req.params.id)  },
+				{ $set : { titulo : req.body.titulo}},
+				{},
+				function(err, results){
+
+					if(err){
+						res.json(err);
+					}else {
+						res.json(results);
+					}
+					mongoclient.close();
+					}
+				);				
+			});
+		});
+	});
 	
 
 
